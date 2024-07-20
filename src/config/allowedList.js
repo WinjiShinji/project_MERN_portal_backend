8054254d72720af2e8fde6c4573242acaa9c20ba
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-07-20 07:04:42 +0100

    Published

--- src/config/allowedList.js
+++ src/config/allowedList.js
@@ -1,6 +1,7 @@
 const allowedList = [
-  'http://localhost:3000',
-  'https://localhost:3000',
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "https://fantastic-heliotrope-6f3230.netlify.app/",
 ]
 
 module.exports = allowedList
