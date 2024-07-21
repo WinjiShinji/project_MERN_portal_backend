de61f117c58c3b93c533cc6f7f3feae0b963ffc9
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-07-21 10:34:11 +0100

    updatedAllowedListRender

--- src/config/allowedList.js
+++ src/config/allowedList.js
@@ -1,6 +1,9 @@
 const allowedList = [
   "https://fantastic-heliotrope-6f3230.netlify.app/",
   "https://project-mern-portal-back-end.onrender.com/",
+  "52.41.36.82",
+  "54.191.253.12",
+  "44.226.122.3",
 ]
 
 module.exports = allowedList
